ce98ff35cbb418d1fa0c1e4191adbd2ec8810b98
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2019-02-18 11:19:52 +0100

    Add camera choice to fovis_track

--- src/fovis_track.cpp
+++ src/fovis_track.cpp
@@ -9,25 +9,90 @@ namespace fs = boost::filesystem;
 
 // This file is automatically formatted with clang-format.
 
-const std::string usage = "Usage: ./fovis_track associations_file";
+const std::string usage =
+    "Usage: ./fovis_track [fr1|fr2|fr3|icl] associations_file";
 
-// Function checking that input arguments are valid.
-fs::path check_arguments(int argc, char **argv) {
+// Generate camera intrinsics parameters.
+fovis::CameraIntrinsicsParameters create_camera(std::string camera_id) {
+  fovis::CameraIntrinsicsParameters camParams;
+  camParams.width = 640;
+  camParams.height = 480;
 
-  // Check that the number of arguments is correct.
-  if (argc != 2) {
+  if (camera_id == "fr1") {
+    camParams.fx = 517.306408;
+    camParams.fy = 516.469215;
+    camParams.cx = 318.643040;
+    camParams.cy = 255.313989;
+    camParams.k1 = 0.262383;
+    camParams.k2 = -0.953104;
+    camParams.p1 = -0.005358;
+    camParams.p2 = 0.002628;
+    camParams.k3 = 1.163314;
+  } else if (camera_id == "fr2") {
+    camParams.fx = 520.908620;
+    camParams.fy = 521.007327;
+    camParams.cx = 325.141442;
+    camParams.cy = 249.701764;
+    camParams.k1 = 0.231222;
+    camParams.k2 = -0.784899;
+    camParams.p1 = -0.003257;
+    camParams.p2 = -0.000105;
+    camParams.k3 = 0.917205;
+  } else if (camera_id == "fr3") {
+    // Color images of Freiburg 3 sequences have already been undistorted.
+    camParams.fx = 535.433105;
+    camParams.fy = 539.212524;
+    camParams.cx = 320.106653;
+    camParams.cy = 247.632132;
+    camParams.k1 = 0.0;
+    camParams.k2 = 0.0;
+    camParams.p1 = 0.0;
+    camParams.p2 = 0.0;
+    camParams.k3 = 0.0;
+  } else if (camera_id == "icl") {
+    camParams.fx = 481.2;
+    camParams.fy = -480.0;
+    camParams.cx = 319.5;
+    camParams.cy = 239.5;
+    camParams.k1 = 0.0;
+    camParams.k2 = 0.0;
+    camParams.p1 = 0.0;
+    camParams.p2 = 0.0;
+    camParams.k3 = 0.0;
+  } else {
+    std::cerr << "Unknown camera id: " << camera_id << std::endl;
     std::cerr << usage << std::endl;
     std::exit(-1);
   }
 
+  return camParams;
+}
+
+struct Checked {
+  fovis::CameraIntrinsicsParameters intrinsics;
+  fs::path associations_file_path;
+};
+
+// Function checking that input arguments are valid.
+Checked check_arguments(int argc, char **argv) {
+
+  // Check that the number of arguments is correct.
+  if (argc != 3) {
+    std::cerr << usage << std::endl;
+    std::exit(-1);
+  }
+
+  // Check that the camera id is correct
+  fovis::CameraIntrinsicsParameters camParams = create_camera(argv[1]);
+
   // Check that the associations file exists.
-  const fs::path associations_file = fs::path(argv[1]);
+  const fs::path associations_file = fs::path(argv[2]);
   if (!fs::is_regular_file(associations_file)) {
     std::cerr << associations_file << " is not a valid file." << std::endl;
     std::exit(-1);
   }
 
-  return associations_file;
+  return {camParams, associations_file};
 }
 
 struct Assoc {
@@ -58,37 +123,6 @@ std::vector<Assoc> extract_associations(fs::path associations_file_path) {
   }
 
   return associations;
-}
-
-// Generate ICL-NUIM camera intrinsics parameters.
-fovis::CameraIntrinsicsParameters create_camera() {
-  fovis::CameraIntrinsicsParameters camParams;
-  camParams.width = 640;
-  camParams.height = 480;
-
-  // fr1
-  camParams.fx = 517.306408;
-  camParams.fy = 516.469215;
-  camParams.cx = 318.643040;
-  camParams.cy = 255.313989;
-  camParams.k1 = 0.262383;
-  camParams.k2 = -0.953104;
-  camParams.p1 = -0.005358;
-  camParams.p2 = 0.002628;
-  camParams.k3 = 1.163314;
-
-  // // icl-nuim
-  // camParams.fx = 481.2;
-  // camParams.fy = -480.0;
-  // camParams.cx = 319.5;
-  // camParams.cy = 239.5;
-  // camParams.k1 = 0.0;
-  // camParams.k2 = 0.0;
-  // camParams.p1 = 0.0;
-  // camParams.p2 = 0.0;
-  // camParams.k3 = 0.0;
-
-  return camParams;
 }
 
 // Retrieve the gray image from file.
@@ -127,15 +161,14 @@ void write_pose(Eigen::Isometry3d pose) {
 int main(int argc, char **argv) {
 
   // Verify that the arguments are correct.
-  fs::path associations_file_path = check_arguments(argc, argv);
+  Checked checked = check_arguments(argc, argv);
 
   // Build a vector containing timestamps and full paths of images.
   std::vector<Assoc> associations =
-      extract_associations(associations_file_path);
+      extract_associations(checked.associations_file_path);
 
   // Initialize intrinsics camera parameters.
-  fovis::CameraIntrinsicsParameters camera_intrinsics = create_camera();
-  fovis::Rectification rectification(camera_intrinsics);
+  fovis::Rectification rectification(checked.intrinsics);
 
   // If we wanted to play around with the different VO parameters, we could set
   // them here in the "options" variable.
@@ -156,9 +189,10 @@ int main(int argc, char **argv) {
     // Load the depth image.
     std::vector<float> depth_data =
         depth_map_from_file(assoc.depth_file_path.string());
-    int depth_width = camera_intrinsics.width;
-    int depth_height = camera_intrinsics.height;
-    fovis::DepthImage depth_image(camera_intrinsics, depth_width, depth_height);
+    int depth_width = checked.intrinsics.width;
+    int depth_height = checked.intrinsics.height;
+    fovis::DepthImage depth_image(checked.intrinsics, depth_width,
+                                  depth_height);
     depth_image.setDepthImage(&depth_data[0]);
 
     // Process the frame.
