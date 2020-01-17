af61a6a1951eb4cff9bf430ca7a315c396b5856c
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2020-01-17 21:13:53 +0100

    Add docker run instruction also in build test

--- dockerfiles/test-build.sh
+++ dockerfiles/test-build.sh
@@ -8,6 +8,9 @@ set -o nounset
 # Build docker container with:
 # docker build -t rgbd-track-eval -f dockerfiles/Dockerfile-ubuntu-16-04 .
 
+# Mount docker container with:
+# docker run --rm -it -v $DATASET:/dataset -v (pwd):/app rgbd-track-eval
+
 cd /app/third-party/fovis
 rm -rf build && mkdir build && cd build && cmake -DCMAKE_BUILD_TYPE=Release .. && make -j2
 
